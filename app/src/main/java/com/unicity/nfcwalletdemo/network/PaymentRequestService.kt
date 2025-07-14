7b1d2d1f02884f5efc1ffe2865de900639b1b0f7
Pavel Grigorenko <pgrigorenko@gmail.com>
2025-07-15 01:08:29 +0400

    sending to qr

---
+++ app/src/main/java/com/unicity/nfcwalletdemo/network/PaymentRequestService.kt
@@ -0,0 +1,31 @@
+package com.unicity.nfcwalletdemo.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
+
+object PaymentRequestService {
+    private const val BASE_URL = "http://10.0.2.2:3001/" // For Android emulator
+    // For physical device on same network, use actual IP like "http://192.168.1.100:3001/"
+    
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .connectTimeout(10, TimeUnit.SECONDS)
+        .readTimeout(10, TimeUnit.SECONDS)
+        .writeTimeout(10, TimeUnit.SECONDS)
+        .build()
+    
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    
+    val api: PaymentRequestApi = retrofit.create(PaymentRequestApi::class.java)
+}
